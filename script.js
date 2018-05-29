f75d47805799bad4285d765b00a2899e8f4d50cb
Olessia Potapova <olessia.potapova@gmail.com>
2018-05-29 09:52:39 -0500

    added ajax api request

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener('click', function() {
-    const quoteItem = document.createElement('P');
-    const newQuote = document.createTextNode("new quote here");
-    quoteItem.appendChild(newQuote);
-    document.getElementById("quote-div").appendChild(quoteItem);
-
-})
+$(document).ready(function() {
+    $('#quote-btn').click(function() {
+        $.ajax({
+            url: 'https://talaikis.com/api/quotes/random/',
+            request: 'GET',
+            data: {format: 'json' },
+            success: function(data) {
+                $('#quote').html(data.quote);
+                $('#author').html(data.author);
+                console.log(data.quote);
+                console.log(data.author);
+            },
+            error: function(){
+                console.log("an error occured");
+            } 
+        });
+    });
+});
